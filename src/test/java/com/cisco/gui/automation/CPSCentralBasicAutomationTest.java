79fe8d4149d5815b41890688b773143df894165d
NishantKansal17 <39698396+NishantKansal17@users.noreply.github.com>
2019-06-30 20:43:44 -0700

    Adding import/export test cases

--- src/test/java/com/cisco/gui/automation/CPSCentralBasicAutomationTest.java
+++ src/test/java/com/cisco/gui/automation/CPSCentralBasicAutomationTest.java
@@ -32,7 +32,7 @@ public class CPSCentralBasicAutomationTest {
 
 	  private WebDriver driver;
 	  private Selenium selenium;
-	  private String fileDownloadPath = "C:/Program Files (x86)/Jenkins/workspace/cps-gui-automation";
+	  private String fileDownloadPath = "/Users/nikansal/Downloads/";
 
 	  static {
 	    System.setProperty( "webdriver.firefox.bin", "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe" );
